c5d64db2cb4f7a6dd37d7bdd49f1f04e4423e8a3
khemPha <pha.khem@amkcambodia.com>
2025-09-04 13:38:43 +0700

    Connnect the fronted and backend

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,15 +1,20 @@
 import express from 'express';
 import type { Request, Response } from 'express';
-import dotenv from 'dotenv';    
+import dotenv from 'dotenv';
 dotenv.config();
 
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req:Request, res:Response) => {
+app.get('/', (req: Request, res: Response) => {
   res.send(process.env.OPENAI_API_KEY);
 });
+
+app.get('/api/hello', (req: Request, res: Response) => {
+  res.json({ message: 'Hello, World!ww' });
+});
+
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
