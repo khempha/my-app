658048f5f05e3575011f89c858e9901aa497f790
khemPha <pha.khem@amkcambodia.com>
2025-09-05 16:50:43 +0700

    Building Chat

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,9 +1,14 @@
 import express from "express";
 import type { Request, Response } from "express";
 import dotenv from "dotenv";
-dotenv.config();
+import OpenAI from "openai";  
 
+dotenv.config();
+const clinet = new OpenAI({
+   apiKey: process.env.OPENAI_API_KEY,
+});
 const app = express();
+app.use(express.json());   
 const port = process.env.PORT || 3000;
 
 app.get("/", (req: Request, res: Response) => {
@@ -12,6 +17,17 @@ app.get("/", (req: Request, res: Response) => {
 
 app.get("/api/hello", (req: Request, res: Response) => {
    res.json({ message: "Hello, World!ww" });
+});
+
+app.post("/api/chat", async (req: Request, res: Response) => {
+   const {prompt} = req.body;
+   const response = await clinet.responses.create({
+      model:"gpt-4o-mini",
+      input:prompt,
+      temperature:0.2,
+      max_output_tokens:100
+   });
+   res.json({ message: response.output_text});
 });
 
 app.listen(port, () => {
